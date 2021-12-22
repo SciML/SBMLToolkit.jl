36e4d57b5d99006bb1a28fdf4c70399a3b0a98b3
anand jain <anandj@uchicago.edu>
2021-12-22 08:52:50 -0800

    fix checksupport

--- src/reactionsystem.jl
+++ src/reactionsystem.jl
@@ -28,7 +28,7 @@ end
 
 """ Check if conversion to ReactionSystem is possible """
 function checksupport(filename::String)
-    not_implemented = ["listOfRules", "listOfConstraints"]
+    not_implemented = ["listOfConstraints"]
     sbml = open(filename) do file
         read(file, String)
     end
