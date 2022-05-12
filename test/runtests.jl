0c3a406db4a767d2cafe6819e9eadd46533ef56f
paulflang <paul.lang@wolfson.ox.ac.uk>
2022-05-12 17:28:16 +0100

    add Wuschel test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 using SBML
 using Catalyst
+using Downloads
 using ModelingToolkit
 using OrdinaryDiffEq
 
@@ -10,4 +11,5 @@ using OrdinaryDiffEq
     @testset "Model to MTK conversions" begin include("reactionsystem.jl") end
     @testset "Rules" begin include("rules.jl") end
     @testset "Events" begin include("events.jl") end
+    @testset "Wuschel" begin include("wuschel.jl") end
 end
