425fc66769e315347bb728055fe5b82531e60d91
paulflang <paul.lang@wolfson.ox.ac.uk>
2022-07-03 12:37:51 +0100

    use SBMLToolkitTestSuite to run misc.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ using SafeTestsets, Test
     @safetestset "Rules" begin include("rules.jl") end
     @safetestset "Events" begin include("events.jl") end
     @safetestset "Utils" begin include("utils.jl") end
-    # @safetestset "Misc" begin include("misc.jl") end
+    @safetestset "Misc" begin include("misc.jl") end
     @safetestset "Wuschel" begin include("wuschel.jl") end
 end
