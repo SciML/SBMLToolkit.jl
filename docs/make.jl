7f6cf630497178ee538380a95c3cf3a1d1d073a0
ArnoStrouwen <arno.strouwen@telenet.be>
2023-02-14 23:05:07 +0100

    various doc and style improvements

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,16 @@ makedocs(;
                                   prettyurls = get(ENV, "CI", "false") == "true",
                                   canonical = "https://docs.sciml.ai/SBMLToolkit/stable/",
                                   assets = ["assets/favicon.ico"]),
+         clean = true, doctest = false, linkcheck = true,
+         linkcheck_ignore = ["https://www.linkedin.com/in/paul-lang-7b54a81a3/"],
+         strict = [
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
          pages = [
              "Home" => "index.md",
              "API documentation" => "api.md",
