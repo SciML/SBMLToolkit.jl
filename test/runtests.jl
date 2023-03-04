12580d63c0048efda31fc6eec8cd6c75a582afe0
Paul Lang <47160416+paulflang@users.noreply.github.com>
2023-03-04 20:53:39 +0100

    run all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using SafeTestsets, Test
 
 @safetestset "SBMLToolkit.jl" begin
-    # @safetestset "Systems" begin include("systems.jl") end
+    @safetestset "Systems" begin include("systems.jl") end
     @safetestset "Reactions" begin include("reactions.jl") end
-    # @safetestset "Rules" begin include("rules.jl") end
-    # @safetestset "Events" begin include("events.jl") end
-    # @safetestset "Utils" begin include("utils.jl") end
-    # @safetestset "Simulation results" begin include("simresults.jl") end
-    # @safetestset "Wuschel" begin include("wuschel.jl") end
+    @safetestset "Rules" begin include("rules.jl") end
+    @safetestset "Events" begin include("events.jl") end
+    @safetestset "Utils" begin include("utils.jl") end
+    @safetestset "Simulation results" begin include("simresults.jl") end
+    @safetestset "Wuschel" begin include("wuschel.jl") end
 end
